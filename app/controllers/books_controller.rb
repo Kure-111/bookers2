ec847bd59e7d588d1803e24f481bc14d9fb974f5
KaitoKuretake <130481064+Kure-111@users.noreply.github.com>
2023-07-06 08:57:44 +0000

    save

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update, :destroy, :index]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy, :index,:show]
   before_action :correct_user, only: [:edit, :update]
+
   def new
   end
 
@@ -8,7 +9,6 @@ class BooksController < ApplicationController
     @books = Book.all
     @user = current_user
     @book = Book.new
-
   end
 
   def show
@@ -17,9 +17,10 @@ class BooksController < ApplicationController
     @books = Book.all
 
   end
-  def top
 
+  def top
   end
+
   def destroy
     @book = Book.find(params[:id])
     user = @book.user
@@ -31,42 +32,45 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  def create
-  @book = Book.new(book_params)
-  @book.user_id = current_user.id
-  @user = current_user
-  if @book.save
-    flash[:notice] = "You have created book successfully."
-    @user = @book.user
-    @books = Book.all
-    redirect_to book_path(@book)
-  else
-    @books = Book.all
-    render :index
-  end
-  end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @user = current_user
+
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      @user = @book.user
+      @books = Book.all
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
 
   def update
-    @book = Book.find(params[:id])
-    if @book.update(book_params)
-      flash[:notice] = "You have updated book successfully."
-      redirect_to book_path(@book.id)
-    else
-      render :edit
-    end
+  @book = Book.find(params[:id])
+  if @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
+    redirect_to book_path(@book.id)
+  else
+
+    render :edit
+  end
   end
 
 
   private
+
   def book_params
-  params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
-   def correct_user
+
+  def correct_user
     @book = Book.find(params[:id])
     unless @book.user_id == current_user.id
       redirect_to books_path
     end
   end
-
 end
